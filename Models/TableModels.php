98bf6da856db7ac77656305a7558ddb6a91896ea
fadelBayloun <fadel.bayloun12@gmail.com>
2023-08-10 16:01:44 +0300

    tableModels done

--- Models/TableModels.php
+++ Models/TableModels.php
@@ -18,5 +18,29 @@ class Classes extends Model{
 class Calendar extends Model{
     protected $tableName = "calendar";
 }
+class News extends Model
+{
+    protected $tableName = "news";
+}
+class Notifications extends Model
+{
+    protected $tableName = "notifications";
+}
+class Subjects extends Model
+{
+    protected $tableName = "subjects";
+}
+class Teachers extends Model
+{
+    protected $tableName = "teachers";
+}
+class UserClasses extends Model
+{
+    protected $tableName = "userclasses";
+}
+class Users extends Model
+{
+    protected $tableName = "users";
+}
 
 ?>
