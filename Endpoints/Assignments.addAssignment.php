bab7a31fabf016d389787974dad85f3bc47564ba
fadelBayloun <fadel.bayloun12@gmail.com>
2023-08-20 15:18:42 +0300

    i fixed update function in the controller class

---
+++ Endpoints/Assignments.addAssignment.php
@@ -0,0 +1,11 @@
+<?php
+include "../Autoloader/autoloader.php";
+$assignmentController= new AssignmentsController();
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $data = file_get_contents("php://input");
+    $data = json_decode($data,true);
+    $result = $assignmentController->insert($data);
+    if($result)echo json_encode("insert succeeded");
+    else echo json_encode("insert failed");
+}
