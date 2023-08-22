514a5a6c1fd59e12127468833d2888fcc18eb768
fadelBayloun <fadel.bayloun12@gmail.com>
2023-08-22 16:32:57 +0300

    finished the remaining selects

---
+++ Endpoints/UserClasses.selectAllUserClasses.php
@@ -0,0 +1,12 @@
+<?php
+include "../Autoloader/autoloader.php";
+$userClassesController = new UserClassesController();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $selectResult = $userClassesController->selectAll();
+
+    $result = [];
+    while ($row = $selectResult->fetch_assoc()) {
+        array_push($result, $row);
+    }
+    echo json_encode($result);
+}
---
+++ Endpoints/UserClasses.selectWhereUserClasses.php
@@ -0,0 +1,27 @@
+<?php
+include "../Autoloader/autoloader.php";
+$userClassesController = new UserClassesController();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $data = file_get_contents("php://input");
+    $data = json_decode($data, true);
+    $condition = "";
+    foreach ($data as $key => $value) {
+        $condition .= "`" . $key . "`='" . $value . "' and ";
+    }
+    $condition = substr($condition, 0, strlen($condition) - 4);
+
+    $selectResult = $userClassesController->selectWhere($condition);
+    $result = [];
+    while ($row = $selectResult->fetch_assoc()) {
+        array_push($result, $row);
+    }
+    echo json_encode($result);
+}
+
+/*
+data:
+{
+    "classid": 1,
+    "userid": 1
+}
+*/
